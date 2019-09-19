271d6931507ba6b9766fc39afd94a07148540123
wellington.melo <wellington.melo@CPX-NIF3W4MHANW.dir.svc.accenture.com>
2019-09-18 23:03:14 -0300

    Fix

--- src/main/java/com/wellington/applications/BootstrapApplication.java
+++ src/main/java/com/wellington/applications/BootstrapApplication.java
@@ -2,8 +2,10 @@ package com.wellington.applications;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BootstrapApplication {
 
 	public static void main(String[] args) {
