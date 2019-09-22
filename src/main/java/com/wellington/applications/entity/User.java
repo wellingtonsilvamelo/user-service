c09900dc36cec9bfdf03dee269cd3e5f149ed78a
wellington.melo <wellington.melo@CPX-NIF3W4MHANW.dir.svc.accenture.com>
2019-09-22 14:07:00 -0300

    Ajuste no banco

--- src/main/java/com/wellington/applications/entity/User.java
+++ src/main/java/com/wellington/applications/entity/User.java
@@ -6,56 +6,67 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-@Table(name = "tbg_user", uniqueConstraints={@UniqueConstraint(columnNames ={"wa_email","wa_username"})})
+@Table(name = "TBG_USER"/*, uniqueConstraints={@UniqueConstraint(columnNames ={"USER_EMAIL","USERNAME"})}*/)
 public class User {
-	
+		
 	@Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-	@Column(name="wa_user_id")
+	@Column(name="USER_ID")
 	private Long userId;
 	
-	@Column(name="wa_name", length=100, nullable=false)
-	private String fullname;
-	
-	@Column(name="wa_username", length=50, nullable=false)
+	@Column(name="USERNAME", length=50, nullable=false)
 	private String username;
 	
-	@Column(name="wa_email", length=70, nullable=false)
+	@Column(name="USER_FULL_NAME", length=100, nullable=false)
+	private String fullname;
+	
+	@Column(name="USER_EMAIL", length=70, nullable=false)
 	private String email;
 	
-	@Column(name="wa_password", length=100, nullable=false)
+	@JsonIgnore
+	@Column(name="USER_HASH_PASSWORD")
 	private String password;
-	
+
 	public Long getUserId() {
 		return userId;
 	}
+
 	public void setUserId(Long userId) {
 		this.userId = userId;
 	}
-	public String getFullname() {
-		return fullname;
-	}
-	public void setFullname(String fullname) {
-		this.fullname = fullname;
-	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
